67ac8b71d0ba80366425da3e6502330a7bef0f71
Harshal Gaulkar <141926540+harshalgaulkar@users.noreply.github.com>
2026-01-27 23:04:55 +0530

    Delete IMSBackend directory

--- IMSBackend/utils/config.js
+++
@@ -1,8 +0,0 @@
-// Configuration helpers for IMSBackend
-// Use environment variable JWT_SECRET in production; fallback to a default for development.
-const config = {
-    SALT_ROUND: 10,
-    SECRET: 'dbwqondewu9bbfsdcdbcknsubdfisbczjvcwuecuivjsdcbjhdhcdw'
-}
-
-module.exports = config
